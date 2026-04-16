d4f6b08bffc4f7fc68c50ded91dd117656a7e250
anika-chhoa <chhoa.anika07@gmail.com>
2026-04-16 18:32:48 +0600

    Loading spinner added to AllFriends section

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 
+import { Suspense } from "react";
 import Banner from "./components/homepage/Banner";
 import AllFriends from "./components/homepage/friendSection/AllFriends";
 import FriendsCount from "./components/homepage/friendSection/FriendsCount";
+import { PuffLoader } from "react-spinners";
 
 
 
@@ -11,7 +13,10 @@ export default function Home() {
       <div className="max-w-7xl mx-auto">
       <Banner/>
       <FriendsCount/>
+      <Suspense fallback={<div className="flex justify-center items-center my-20"><PuffLoader color="green" /></div>}>
+
       <AllFriends/>
+      </Suspense>
     </div>
     </div>
   );
